e8e2ed6f88e4b0105aba80be89c3280930e2da94
Tom Pullen <tpullen21@gmail.com>
2016-01-13 15:06:52 +0000

    Added ability to view reviews, including total and averaging functionality

--- client/templates/products/layout.js
+++ client/templates/products/layout.js
@@ -8,3 +8,22 @@ Template.registerHelper('truncateText', function(text, length) {
 
 	return new Spacebars.SafeString(truncatedText);
 });
+
+// Get Average Rating
+Template.registerHelper('getAvg', function (reviews) {
+    var sum = 0;
+    for (var i = 0; i < reviews.length; i++) {
+        sum += parseInt(reviews[i].rating, 10);
+    }
+    var avg = sum / reviews.length;
+    return Math.round(avg);
+});
+
+// Get Reviews Total
+Template.registerHelper('getReviewsTotal', function (total) {
+   if(total > 0){
+    return total;
+   } else {
+    return 0;
+   }
+});
